4af8398c2417b9deadc9162fa39c8bdde061d000
Amy Langley <amy.langley@tempus.com>
2023-03-16 17:59:25 -0500

    start working towards neutral corpus

--- retrieval/legiscan.py
+++ retrieval/legiscan.py
@@ -4,33 +4,34 @@ import os
 import requests
 from typing import Optional
 
-def legiscan_auth(authorized_action):
+def make_legiscan_session() -> requests.Session:
     FORM_URL = 'https://legiscan.com/user/login'
     CREDENTIAL_DATA = {
         'name': os.environ.get("LEGISCAN_USERNAME"),
         'pass': os.environ.get("LEGISCAN_PASSWORD"),
     }
 
-    def make_legiscan_session() -> requests.Session:
-        session = requests.Session()
-        get_form = session.get(FORM_URL)
-        soup = Soup(get_form.content, features="html.parser")
+    session = requests.Session()
+    get_form = session.get(FORM_URL)
+    soup = Soup(get_form.content, features="html.parser")
 
-        login_form = soup.find(id='user-login')
-        form_build_id = login_form.find('input', {'name': 'form_build_id'}).get('value')
-        form_id = login_form.find('input', {'name': 'form_id'}).get('value')
-        op = login_form.find('input', {'name': 'op'}).get('value')
+    login_form = soup.find(id='user-login')
+    form_build_id = login_form.find('input', {'name': 'form_build_id'}).get('value')
+    form_id = login_form.find('input', {'name': 'form_id'}).get('value')
+    op = login_form.find('input', {'name': 'op'}).get('value')
 
-        post_data = {
-            **CREDENTIAL_DATA,
-            'form_build_id': form_build_id,
-            'form_id': form_id,
-            'op': op,
-        }
+    post_data = {
+        **CREDENTIAL_DATA,
+        'form_build_id': form_build_id,
+        'form_id': form_id,
+        'op': op,
+    }
 
-        session.post(FORM_URL, data=post_data)
-        return session
-    
+    session.post(FORM_URL, data=post_data)
+    LEGISCAN_SESSION = session
+    return session
+
+def legiscan_auth(authorized_action):
     @wraps(authorized_action)
     def authorization_wrapper(*args, **kwargs):
         if 'session' in kwargs:
