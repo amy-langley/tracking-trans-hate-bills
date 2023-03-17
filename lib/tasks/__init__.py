6f864da2f144466751bd772e406bb37dd2f56268
Amy Langley <amy.langley@tempus.com>
2023-03-17 17:46:47 -0500

    start building corpus generator

--- lib/tasks/__init__.py
+++ lib/tasks/__init__.py
@@ -1,5 +1,6 @@
 from .extract_legiscan_datasets import extract_legiscan_datasets
 from .get_legiscan_datasets import get_legiscan_datasets
+from .prepare_neutral_corpus import prepare_neutral_corpus
 from .process_legiscan_datasets import process_legiscan_datasets
 
 from .tokenize_corpus import tokenize_corpus
---
+++ lib/tasks/prepare_neutral_corpus.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from typing import List
+
+
+def select_neutral_corpus(summaries_file: str, corpus_size: int, *, filter_states: List[str]=[], random_state: int=1) -> pd.DataFrame:
+    summaries = pd.read_json(summaries_file)
+    candidates = summaries.apply(
+        lambda x: (x['legiscan_doc_id'] is not None) and ((not filter_states) or x['state'] in filter_states),
+        axis=1,
+    )
+    return summaries.loc[candidates].sample(n=corpus_size, random_state=random_state)
+    
+
+
+def prepare_neutral_corpus(work_dir: str, summaries_file: str, corpus_size: int, *, filter_states: List[str]=[], random_state: int=1): # -> None:
+    return select_neutral_corpus(summaries_file, corpus_size, filter_states=filter_states, random_state=random_state)
+
