9d09dc3588b3c8ef3ae0e588c1829a0ca183573f
Amy Langley <amy.langley@tempus.com>
2023-03-17 13:34:48 -0500

    refactor language operations

---
+++ lib/language/text_operations.py
@@ -0,0 +1,20 @@
+from nltk import word_tokenize as tokenize_text
+import re
+
+
+def clean_text(text: str) -> str:
+    no_entities = (
+        text
+            .replace('—', '-')
+            .replace('“', '"')
+            .replace('”', '"')
+            .replace('’', '\'')
+    )  # remove annoying HTML entities and things like that
+    
+    resolve_hyphens = re.subn(
+        r'([A-Za-z]+)\s*-\s*\n\s*(\d+\s*)?([A-Za-z]+)',
+        r'\g<1>\g<3>\n',
+        no_entities
+    )[0]  # subn returns a tuple including number of matches
+    
+    return resolve_hyphens
---
+++ lib/language/token_operations.py
@@ -0,0 +1,68 @@
+from nltk import pos_tag
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+import re
+import string
+from typing import Iterable, List, Tuple
+
+
+TaggedToken = Tuple[str, str]
+TokenList = List[str]
+TokenStream = Iterable[str]
+TaggedTokenStream = Iterable[TaggedToken]
+
+
+lem = WordNetLemmatizer()
+
+
+def _get_wordnet_pos(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
+
+
+def clean_tokens(token_stream: TokenStream) -> TokenStream:
+    return (
+        outer_token
+        for outer_token
+        in (
+            token.lower().strip(string.punctuation)
+            for token
+            in token_stream
+        )
+        if (
+            outer_token
+            and outer_token not in string.punctuation
+            and not re.search('\d', outer_token)
+        )
+    )
+
+def retag_for_wordnet(tagged_token_stream: TaggedTokenStream) -> TaggedTokenStream:
+    return (
+        (word, _get_wordnet_pos(tag))
+        for word, tag
+        in tagged_token_stream
+    )
+
+
+# because POS tagging requires us to provide texts (so it can grab context),
+# we have to eagerly evaluate the stream before canonicalizing
+def canonicalize_tokens(token_list: TokenList) -> TokenStream:
+    tagged = retag_for_wordnet(
+        tagged_pair
+        for tagged_pair
+        in pos_tag(list(token_list))
+    )
+
+    return (
+        lem.lemmatize(*tagpair)
+        for tagpair
+        in tagged
+    )
