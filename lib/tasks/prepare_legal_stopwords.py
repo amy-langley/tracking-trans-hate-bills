83799e226e2a85be69c0644ec9d4bd9b216cb116
Amy Langley <amy.a.langley@gmail.com>
2023-03-17 20:29:05 -0500

    prepare stopwords task

---
+++ lib/tasks/prepare_legal_stopwords.py
@@ -0,0 +1,21 @@
+import json
+from nltk import FreqDist
+from typing import List
+
+def prepare_legal_stopwords(token_path: str, output_filename: str) -> List[str]:
+    with open(token_path, 'r') as f:
+        canonicalized_tokens = json.load(f)
+
+    distribution = FreqDist(canonicalized_tokens)
+    
+    stopwords = list(
+        k
+        for k, v
+        in distribution.items()
+        if v > 800 and len(k) > 1
+    )
+    
+    with open(output_filename, 'w') as f:
+        json.dump(stopwords, f, indent=2)
+    
+    return stopwords
