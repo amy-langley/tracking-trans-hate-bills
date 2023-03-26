ef84ecf59c58af4564d104064d3a472e3cf95005
Amy Langley <amy.a.langley@gmail.com>
2023-03-25 21:16:46 -0500

    better animated coloration

--- lib/tasks/visualization/generate_animated_choropleth.py
+++ lib/tasks/visualization/generate_animated_choropleth.py
@@ -74,7 +74,14 @@ def generate_animated_choropleth(
         color_continuous_scale="reds",
         locationmode='USA-states',
         scope="usa",
-        range_color=(0, 3*(choropleth_data['ct'].median()) + national_data['ct'].max()),
+        range_color=(
+            0,
+            (
+                choropleth_data['ct'].mean()
+                + choropleth_data['ct'].std() * 4
+                + national_data['ct'].max()
+            )
+        ),
         title='Anti-Trans Bills by State',
         height=600
     )
